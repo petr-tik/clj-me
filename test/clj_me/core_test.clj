99dc0e178f2feb562cda2429976778e93d74836a
eleanormann <eleanor@wefarm.org>
2018-05-14 20:42:37 +0100

    added spec

--- test/clj_me/core_test.clj
+++ test/clj_me/core_test.clj
@@ -1,7 +1,25 @@
 (ns clj-me.core-test
   (:require [clojure.test :refer :all]
-            [clj-me.core :refer :all]))
+            [clj-me.core :refer :all]
+            [clojure.spec.test.alpha :as stest]
+            [clojure.spec.alpha :as s]
+            ))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+(s/def ::side boolean?)
+(s/def ::amount int?)
+(s/def ::price int?)
+
+(s/def ::order (s/keys :req [::side ::amount ::price]))
+
+(defn order-crunch
+  [order]
+  order)
+
+(s/fdef order-crunch
+        :args int?
+        :ret int?
+        :fn #(= (:ret %) (:args %)))
+
+
+(stest/check `order-crunch)
