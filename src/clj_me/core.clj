38604a423dab45aaec9eea539be2c7a69df20a5a
Guilherme Gonçalves <ggoncalves@lastline.com>
2018-05-14 20:57:24 +0100

    Add semi-functional version of bucket-orders, create test case

--- src/clj_me/core.clj
+++ src/clj_me/core.clj
@@ -1,5 +1,4 @@
 (ns clj-me.core
-
   (:gen-class))
 
 
@@ -7,24 +6,28 @@
 
 (def sides {:buy 0 :sell 1})
 
-(def generate-order 
-[]
-{:actor (rand-nth actors) 
-:side (rand-nth sides) 
-:amount (inc (rand-int 100)) :price ((inc (rand-int 50)))}
-)
+(def test-orders [{:actor "JPMC", :side 1, :amount 34, :price 7} {:actor "BAML", :side 0, :amount 96, :price 40} {:actor "BAML", :side 0, :amount 10, :price 1} {:actor "BAML", :side 1, :amount 37, :price 11} {:actor "GSCO", :side 0, :amount 2, :price 40}])
+
+(defn generate-order []
+  {:actor (rand-nth actors)
+   :side (rand-nth (vals sides))
+   :amount (inc (rand-int 100)) :price (inc (rand-int 50))})
 
 (defn bucket-orders
-[orders]
-()
-
-)
+  [orders]
+  (let [bucketed-orders {}]
+    (for [order orders
+          :let [price (order :price)
+                price-key (keyword (str price)) ]]
+      (if (contains? bucketed-orders price-key)
+        (update bucketed-orders price-key conj order)
+        (assoc bucketed-orders price-key [order])))))
 
 (defn process-orders
-[orders]
+  [orders]
 
 
-)
+  )
 
 
 (defn -main
