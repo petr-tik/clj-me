b68cc4310fd8958900215cf55e67c272e12f5c62
petr-tik <petr-tik@users.noreply.github.com>
2018-05-14 19:57:23 +0100

    First prototype of ME

--- src/clj_me/core.clj
+++ src/clj_me/core.clj
@@ -1,5 +1,31 @@
 (ns clj-me.core
+
   (:gen-class))
+
+
+(def actors ["GSCO" "BAML" "JPMC"])
+
+(def sides {:buy 0 :sell 1})
+
+(def generate-order 
+[]
+{:actor (rand-nth actors) 
+:side (rand-nth sides) 
+:amount (inc (rand-int 100)) :price ((inc (rand-int 50)))}
+)
+
+(defn bucket-orders
+[orders]
+()
+
+)
+
+(defn process-orders
+[orders]
+
+
+)
+
 
 (defn -main
   "I don't do a whole lot ... yet."
