b6af526b4e0bfba26bc53d77e549512847976ac5
Guilherme Gonçalves <ggoncalves@lastline.com>
2018-05-16 08:54:30 +0100

    Add prototype for transaction matching

--- src/clj_me/core.clj
+++ src/clj_me/core.clj
@@ -1,20 +1,23 @@
 (ns clj-me.core
   (:gen-class))
 
-
 (def actors ["GSCO" "BAML" "JPMC"])
+(def sides [:buy :sell])
+(def test-orders [{:actor "JPMC", :side :sell, :amount 34, :price 7} {:actor "BAML", :side :buy, :amount 96, :price 40} {:actor "BAML", :side :buy, :amount 10, :price 1} {:actor "BAML", :side :sell, :amount 37, :price 11} {:actor "GSCO", :side :sell, :amount 20, :price 40}])
 
-(def sides {:buy 0 :sell 1})
-
-(def test-orders [{:actor "JPMC", :side 1, :amount 34, :price 7} {:actor "BAML", :side 0, :amount 96, :price 40} {:actor "BAML", :side 0, :amount 10, :price 1} {:actor "BAML", :side 1, :amount 37, :price 11} {:actor "GSCO", :side 0, :amount 2, :price 40}])
+(defn make-order [actor amount price]
+  {:actor actor
+   :side (if (pos-int? amount) :buy :sell)
+   :amount amount
+   :price price})
 
 (defn generate-order []
   {:actor (rand-nth actors)
-   :side (rand-nth (vals sides))
-   :amount (inc (rand-int 100)) :price (inc (rand-int 50))})
+   :side (rand-nth sides)
+   :amount (inc (rand-int 100))
+   :price (inc (rand-int 50))})
 
-
-(defn add-new-order
+(defn add-order-to-buckets
   [bucketed-orders-so-far order]
   (let [price-key (keyword (str (:price order)))]
   (if (contains? bucketed-orders-so-far price-key)
@@ -22,16 +25,28 @@
         (assoc bucketed-orders-so-far price-key [order]))))
 
 (defn bucket-orders [orders]
-  (reduce add-new-order {} orders))
+  (reduce add-order-to-buckets {} orders))
+
+(defn process-order [order]
+  (let [amount (:amount order)
+        pick-amount (fn [order] (if (= :buy (:side order)) amount (- amount)))]
+    (assoc order :amount (pick-amount order))))
 
 (defn process-orders [orders]
   (map process-order orders))
 
-(defn process-order [order]
-  (let [price (:price order)]
-    (assoc order :price (if (:side order) price (- price)))))
+;; (defn match-transactions [transactions] (reduce ...)
+
+;;; FIXME: this doesn't behave well when they're the same amount
+;;; TODO: Pick the actor from the transaction with the larger amount
+(defn match-transaction [trans1 trans2]
+  (let [amount1 (:amount trans1)
+        amount2 (:amount trans2)]
+    (if (neg-int? (* amount1 amount2)) ; they must be opposite directions
+      (make-order "FOOBAR" (+ amount1 amount2) (:price trans1))
+      nil)))                      ; nil means not a match
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (print (bucket-orders (process-orders test-orders)))
+  (print (bucket-orders (process-orders test-orders))))
